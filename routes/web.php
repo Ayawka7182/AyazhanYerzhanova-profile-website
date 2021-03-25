2777ebcd8f249433e066b2c98690ef26e2ec4917
Ayazhan <190107082@stu.sdu.edu.kz>
2021-03-25 12:38:22 +0600

    lab5 is done

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,7 @@ Route::get('/post/create', function() {
     ]);
 });
 Route::get('post', [BlogController::class, 'index']);
+Route::get('post/create', function(){
+    return view('post.create');
+});
+Route::post('post/create', [BlogController::class, 'store'])->name('add-client');
